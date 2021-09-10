15676fbb10241b57c07051024dc4d1626c74499a
William <williamhjang@gmail.com>
2021-09-10 17:48:05 -0400

    login page styled v1

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -3,7 +3,7 @@ import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
-import './LoginForm.css';
+import styles from './LoginForm.module.css';
 
 const LoginFormPage = () => {
     const dispatch = useDispatch();
@@ -29,31 +29,39 @@ const LoginFormPage = () => {
     }
 
     return (
-        <div className="log-in">
-            <form className="login-form" onSubmit={onSubmit}>
-                <ul>
-                    {errors.map((error, index) => (
-                        <li className="errors" key={index}> {error} </li>
-                    ))}
-                </ul>
-                <label> Username or Email
-                    <input
-                        type="text"
-                        value={credential}
-                        onChange={(e) => setCrendtial(e.target.value)}
-                        required
-                    />
-                </label>
-                <label> Password
-                    <input
-                        type="text"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </label>
-                <button type="submit">Log In</button>
-            </form>
+        <div className={styles.container}>
+            <div className={styles.login}>
+                <form className={styles.loginForm} onSubmit={onSubmit}>
+                    <h2> UNSLIC'D </h2>
+                    <p> Let's get this slice!</p>
+                    <ul>
+                        {errors.map((error, index) => (
+                            <li className="errors" key={index}> {error} </li>
+                        ))}
+                    </ul>
+                    <div className={styles.loginFormGroup}>
+                        <label> Username or Email
+                            <input
+                                type="text"
+                                value={credential}
+                                onChange={(e) => setCrendtial(e.target.value)}
+                                required
+                            />
+                        </label>
+                    </div>
+                    <div className="login-form-group">
+                        <label> Password
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </label>
+                    </div>
+                    <button type="submit">Log In</button>
+                </form>
+            </div>
         </div>
     );
 }
