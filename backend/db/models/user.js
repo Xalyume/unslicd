e2580fb80f39ffa26efb7985c07bae00599c42cc
William <williamhjang@gmail.com>
2021-09-13 18:07:30 -0400

    began creating components for forms/checkin

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -50,8 +50,11 @@ module.exports = (sequelize, DataTypes) => {
     // associations can be defined here
   };
   User.prototype.toSafeObject = function () { // remember, this cannot be an arrow function
-    const { id, username, email } = this; // context will be the User instance
-    return { id, username, email };
+    const { id, username, email, createdAt } = this; // context will be the User instance
+
+    const stringDate = createdAt.toLocaleDateString()
+
+    return { id, username, email, createdAt: stringDate };
   };
   User.prototype.validatePassword = function (password) {
     return bcrypt.compareSync(password, this.hashedPassword.toString());
