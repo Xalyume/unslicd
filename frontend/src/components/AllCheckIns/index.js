e865808ca3f69310da046887a43ce1e264ad7205
William <williamhjang@gmail.com>
2021-11-24 18:01:06 -0500

    working on checkin component

--- frontend/src/components/AllCheckIns/index.js
+++ frontend/src/components/AllCheckIns/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getCheckIn } from '../../store/checkin';
+
+import CheckInCard from '../../components/User/CheckInCard'
 
 import checkin from './AllCheckIns.module.css'
 
 function AllCheckIns() {
+    const dispatch = useDispatch();
+
+    const sessionUser = useSelector(state => state.session.user);
+    const checkIns = useSelector(state => state.checkins);
+
+    const checkinArr = Object.values(checkIns)
+
+    useEffect(() => {
+        dispatch(getCheckIn())
+    }, [dispatch])
+
+    if (!sessionUser) return (
+        <Redirect to='/login' />
+    )
+
+    console.log(checkinArr);
+
     return (
-        <h1>this is for ALL of the check ins</h1>
+        null
     )
 }
 
