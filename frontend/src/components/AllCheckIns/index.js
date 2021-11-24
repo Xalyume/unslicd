09530d5a3d4fa51ac27e86e8aa8687d4ce25304e
William <williamhjang@gmail.com>
2021-11-24 17:23:43 -0500

    initial component created for all checkins

---
+++ frontend/src/components/AllCheckIns/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import checkin from './AllCheckIns.module.css'
+
+function AllCheckIns() {
+    return (
+        <h1>this is for ALL of the check ins</h1>
+    )
+}
+
+export default AllCheckIns;
