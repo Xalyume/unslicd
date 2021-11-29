8f5470de9381cdcc5151fd7555fbbc6aa8d7ffb8
William <williamhjang@gmail.com>
2021-11-29 18:00:50 -0500

    current redux grabs all checkins and filters on front end

--- frontend/src/components/UserPage/index.js
+++ frontend/src/components/UserPage/index.js
@@ -2,11 +2,27 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams, Redirect } from 'react-router-dom';
 
+import { getAllCheckIn } from '../../store/checkin';
+
 function UserPage() {
+    const dispatch = useDispatch();
     const history = useHistory();
     const { userId } = useParams();
 
     const sessionUser = useSelector(state => state.session.user);
+    const checkIns = useSelector(state => state.checkIns);
+
+    const checkInArr = Object.values(checkIns)
+    const userCheckin = checkInArr.filter((element) => {
+
+        return element.userId === parseInt(userId)
+    })
+
+    console.log(userCheckin)
+
+    useEffect(() => {
+        dispatch(getAllCheckIn())
+    }, [dispatch])
 
     if (sessionUser.id === parseInt(userId)) {
         history.push('/')
